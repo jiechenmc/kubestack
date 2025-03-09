15b0b974d75b71d014b46a344dcbcd15fdc8eeb2
Jie Chen <jiechenmc@gmail.com>
2025-03-09 17:28:53 -0400

    update service monitor location

---
+++ k3s.sh
@@ -0,0 +1,8 @@
+# On extra
+# curl -sfL https://get.k3s.io | K3S_TOKEN=SECRET sh -s - server --server https://10.0.0.60:6443 \
+#   --flannel-backend=none \
+#   --disable-kube-proxy \
+#   --disable servicelb \
+#   --disable-network-policy \
+#   --disable traefik \
+#   --cluster-init
