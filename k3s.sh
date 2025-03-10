e5ce1300bc7af27fb6537d3f020a10412e6b0b60
Jie Chen <jiechenmc@gmail.com>
2025-03-09 21:50:10 -0400

    save

--- k3s.sh
+++ k3s.sh
@@ -1,10 +1,9 @@
-# On extra
-# curl -sfL https://get.k3s.io | K3S_TOKEN=SECRET sh -s - server --server https://10.0.0.60:6443 \
-#   --flannel-backend=none \
-#   --disable-kube-proxy \
-#   --disable servicelb \
-#   --disable-network-policy \
-#   --disable traefik \
-#   --cluster-init
+curl -sfL https://get.k3s.io | K3S_TOKEN=SECRET sh -s - server --server https://10.0.0.60:6443 \
+  --flannel-backend=none \
+  --disable servicelb,traefik,local-storage \
+  --disable-kube-proxy \
+  --disable-network-policy \
+  --disable-helm-controller \
+  --cluster-init
 
-kubectl create secret generic cloudflared-token-secret --from-literal=token=$CF_TOKEN
+kubectl create secret generic cloudflared-token-secret --from-literal=token=$CF_TOKEN   
