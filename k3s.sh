317729d9a4ddabab1276f545624db7fed79d0c8b
Jie Chen <jiechenmc@gmail.com>
2025-03-10 18:50:59 -0400

    cilium need to be installed during cluster install

--- k3s.sh
+++ k3s.sh
@@ -1,11 +1,16 @@
-curl -sfL https://get.k3s.io | K3S_TOKEN=SECRET sh -s - server --server https://10.0.0.60:6443 \
-  --flannel-backend=none \
-  --disable servicelb,traefik,local-storage \
-  --disable-kube-proxy \
-  --disable-network-policy \
-  --disable-helm-controller \
-  --cluster-init
+# curl -sfL https://get.k3s.io | K3S_TOKEN=SECRET sh -s - server --server https://10.0.0.60:6443 \
+#   --flannel-backend=none \
+#   --disable servicelb,traefik,local-storage \
+#   --disable-kube-proxy \
+#   --disable-network-policy \
+#   --disable-helm-controller \
+#   --cluster-init
 
-kubectl create secret generic cloudflared-token-secret --from-literal=token=$CF_TOKEN   
+# kubectl create secret generic cloudflared-token-secret --from-literal=token=$CF_TOKEN   
 
 # sudo apt-get install open-iscsi is needed for longhorn
+#cilium install \         
+  # --set k8sServiceHost=10.0.0.60 \
+  # --set k8sServicePort=6443 \
+  # --set kubeProxyReplacement=true \
+  # --set=ipam.operator.clusterPoolIPv4PodCIDRList="10.42.0.0/16"
