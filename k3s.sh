20800e4ac43e4a76e84362f5474c5fa39f14ecbd
Jie Chen <jiechenmc@gmail.com>
2025-03-10 22:42:58 -0400

    remove dups

--- k3s.sh
+++ k3s.sh
@@ -1,10 +1,7 @@
-# curl -sfL https://get.k3s.io | K3S_TOKEN=SECRET sh -s - server --server https://10.0.0.60:6443 \
-#   --flannel-backend=none \
-#   --disable servicelb,traefik,local-storage \
-#   --disable-kube-proxy \
-#   --disable-network-policy \
-#   --disable-helm-controller \
-#   --cluster-init
+curl -sfL https://get.k3s.io | K3S_TOKEN=SECRET sh -s - server --server https://10.0.0.60:6443 \
+  --disable local-storage \
+  --disable-helm-controller \
+  --cluster-init
 
 # kubectl create secret generic cloudflared-token-secret --from-literal=token=$CF_TOKEN   
 
