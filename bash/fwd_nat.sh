225126a3e0d4775cdfddfefcec62cadcda39b751
Jie Chen <jiechenmc@gmail.com>
2025-05-10 15:33:28 -0400

    add firewalld command

--- bash/fwd_nat.sh
+++ bash/fwd_nat.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/bash
+
+TRUSTED_NIC=eno1
+
 sudo sysctl -w net.ipv4.ip_forward=1
-sudo iptables -t nat -A POSTROUTING -o wlp3s0 -s 10.10.0.0/24 -j MASQUERADE
+
+
+# No longer needed because firewalld abstracts the rules
+#sudo iptables -t nat -A POSTROUTING -o $FWD_NIC -s 10.10.0.0/24 -j MASQUERADE
+sudo firewall-cmd --zone=trusted --add-forward --permanent
+sudo firewall-cmd --zone=public --add-masquerade --permanent
+
+sudo firewall-cmd --zone=trusted --add-interface=$TRUSTED_NIC --permanent
+
+sudo firewall-cmd --reload
