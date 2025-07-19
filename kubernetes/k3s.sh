e9362416736cc146c656ff6c525e24dc8e967f27
Jie Chen <jiechenmc@gmail.com>
2025-07-19 05:29:26 +0000

    stuff

--- kubernetes/k3s.sh
+++ kubernetes/k3s.sh
@@ -1,4 +1,5 @@
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+export SERVER_IP="$(ip route get 1.1.1.1 | awk '{print $7}')"
 
 curl -sfL https://get.k3s.io | K3S_TOKEN=SECRET sh -s - server \
   --flannel-backend none \
@@ -7,4 +8,8 @@ curl -sfL https://get.k3s.io | K3S_TOKEN=SECRET sh -s - server \
   --write-kubeconfig-mode 644 \
   --cluster-init
 
-cilium install
+cilium install \
+  --set k8sServiceHost=$SERVER_IP \
+  --set k8sServicePort=6443 \
+  --set kubeProxyReplacement=true \
+  --set=ipam.operator.clusterPoolIPv4PodCIDRList="10.42.0.0/16"
--- kubernetes/k3s_agent.sh
+++ kubernetes/k3s_agent.sh
@@ -8,9 +8,3 @@ curl -sfL https://get.k3s.io | K3S_TOKEN=SECRET sh -s - server --server https://
   --disable-helm-controller \
   --write-kubeconfig-mode 644 \
   --cluster-init
-
-cilium install \         
-  --set k8sServiceHost=$SERVER_IP \
-  --set k8sServicePort=6443 \
-  --set kubeProxyReplacement=true \
-  --set=ipam.operator.clusterPoolIPv4PodCIDRList="10.42.0.0/16"
