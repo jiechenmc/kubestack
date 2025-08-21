128f0d6a2f3fbd9b91fa67d99cf84fe191b88b3f
Jie Chen <jiechenmc@gmail.com>
2025-08-20 20:22:38 -0500

    remove k3s scripts -> use ansible now

--- kubernetes/k3s_agent.sh
+++
@@ -1,8 +0,0 @@
-export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
-
-curl -sfL https://get.k3s.io | K3S_TOKEN=SECRET sh -s - server --server https://$SERVER_IP:6443 \
-  --flannel-backend none \
-  --disable-network-policy \
-  --disable-helm-controller \
-  --write-kubeconfig-mode 644 \
-  --cluster-init
