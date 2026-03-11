2af8a6f37bf203e0428b3a871065ba69a436e965
Jie Chen <jiechenmc@gmail.com>
2026-03-11 18:16:22 -0500

    remove

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,3 @@ kubectl create secret tls local-selfsigned-tls \
   --cert=./tls.crt \
   --key=./tls.key \
   -n traefik
-
-sudo firewall-cmd --permanent --zone=trusted --add-interface=cni0
-sudo firewall-cmd --permanent --zone=trusted --add-interface=flannel.1
-sudo firewall-cmd --reload
