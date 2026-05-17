15ebeeb55e7b79d66d6c7c0e108640b1bcdd5323
Jie Chen <jiechenmc@gmail.com>
2026-05-16 20:08:03 -0500

    UP UP UP

--- install.sh
+++ install.sh
@@ -1,22 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-INVENTORY="ansible/inventory.yaml"
-PLAYBOOK="ansible/site.yaml"
-TLS_CERT="./tls.crt"
-TLS_KEY="./tls.key"
-TLS_SECRET="local-selfsigned-tls"
-TLS_NS="kube-system"
-CHARTS_DIR="charts"
-MANIFESTS="kubernetes/generated"
-
-# ── Activate ansible venv ───────────────────────────────────────
-. ~/.ansible/bin/activate
-
-# ── Run Ansible playbook ────────────────────────────────────────
-echo "── Running Ansible playbook ───────────────────────────────"
-ansible-playbook -i "$INVENTORY" "$PLAYBOOK"
-
 python3 kubestack.py generate
 
 sleep 5s
